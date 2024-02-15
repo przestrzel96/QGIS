812e29508dcfa3f0c39226c8968142db51d65275
przestrzel96 <113241110+przestrzel96@users.noreply.github.com>
2024-02-15 22:38:10 +0100

    Update lengthv2.py

--- lengthv2.py
+++ lengthv2.py
@@ -1,26 +1,24 @@
 from qgis.core import QgsField, QgsExpression, QgsExpressionContext, QgsExpressionContextUtils
 from qgis.gui import QgsMapCanvas
 
-# Pobieramy aktywną warstwę
-layer = iface.activeLayer()
+# Pobieranie aktywnej warstwy
+warstwa = iface.activeLayer()
 
-# Dodajemy nową kolumnę na długość w metrach
-pv = layer.dataProvider()
-pv.addAttributes([QgsField('lenght', QVariant.Double)])
-layer.updateFields()
+# Sprawdzanie, czy istnieje kolumna 'długość', i dodawanie jej, jeśli nie istnieje
+if 'długosc' not in warstwa.fields().names():
+    pv = warstwa.dataProvider()
+    pv.addAttributes([QgsField('długosc', QVariant.Double)])
+    warstwa.updateFields()
 
-# Tworzymy wyrażenie do obliczenia długości
+# Tworzenie wyrażenia do obliczania długości
 expression = QgsExpression('round($length, 4)')
+context = QgsExpressionContext() 
+context.appendScopes(QgsExpressionContextUtils.globalProjectLayerScopes(warstwa))
 
-context = QgsExpressionContext()
-context.appendScopes(QgsExpressionContextUtils.globalProjectLayerScopes(layer))
-
-# Edytujemy warstwę
-with edit(layer):
-    for feature in layer.getFeatures():
-        context.setFeature(feature)
-        # Obliczamy długość z wyrażeniem
-        length_m = expression.evaluate(context)
-        # Zapisujemy długość w kolumnie
-        feature['lenght'] = length_m
-        layer.updateFeature(feature)
+# Edytowanie warstwy i obliczanie długości
+with edit(warstwa):
+    for f in warstwa.getFeatures():
+        context.setFeature(f)
+        długość_m = expression.evaluate(context)
+        f['długosc'] = długość_m
+        warstwa.updateFeature(f)
